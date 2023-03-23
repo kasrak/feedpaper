9d1d3a69ff2ba24ac2f2a99cdb938248099dd325
Kasra Kyanzadeh <kasrak@gmail.com>
2023-03-22 17:30:41 -0700

    basic server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+// Basic Node.js server that returns "Hello World"
+// Run with: node server/index.js
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 3030;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello World");
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
