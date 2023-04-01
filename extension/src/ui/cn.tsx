3777b0442d545cae6d85c46444b8564c356795de
Kasra Kyanzadeh <kasrak@gmail.com>
2023-03-31 17:58:55 -0700

    delete extension

--- extension/src/ui/cn.tsx
+++
@@ -1,7 +0,0 @@
-import { ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-/// Helper for conditionally adding Tailwind classes
-export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
-}
--- extension/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    mode: "jit",
-    content: ["./src/**/*.tsx"],
-    theme: {
-        extend: {},
-    },
-    plugins: [require("tailwindcss-animate")],
-};
